253c97cfb21f1b52853d4c493ca1b9da2e9a3254
laryszB <blazej.larysz001@gmail.com>
2023-05-04 18:36:27 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                laravel: "#8972a8",
+            },
+        },
+    },
+    plugins: [],
+}
